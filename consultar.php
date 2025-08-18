ad2c03fd640e5c116b136cae6046dac5c240fbab
João <joaomartins102009@gmail.com>
2025-08-18 13:28:53 -0300

    Corrigindo erros de sintaxe

--- consultar.php
+++ consultar.php
@@ -8,27 +8,26 @@ echo "<th>SEXO</th>";
 echo "</tr>";
 
 //CONEXÃO COM O BD
-$strcon = mysqli_connect('localhost', 'joao', '123', 'banco_teste')or die('Erro ao Conectar no Banco de Dados')
-$sql = "SELECT * FROM cadastro";
-$resultado = msql_query($strcon, $sql) or die ("Erro ao tentar capturar registros");
+$strcon = mysqli_connect('localhost', 'joao', '123', 'banco_teste') 
+    or die('Erro ao Conectar no Banco de Dados');
 
+//CONSULTA
+$sql = "SELECT * FROM cadastro";
+$resultado = mysqli_query($strcon, $sql) or die("Erro ao tentar capturar registros");
 
 // Obtendo os dados por meio de um loop while
-
 while ($registro = mysqli_fetch_array($resultado)) {
-	$nome = $registro['NomeCliente'];
-	$sobrenome = $registro['SobrenomeCliente'];
-	$sexo = $registro['Sexo'];
+    $nome = $registro['NomeCliente'];
+    $sobrenome = $registro['SobrenomeCliente'];
+    $sexo = $registro['SEXO'];
 
-	echo "<table border=1>";
-	echo "<tr>";
-	echo "<th>".$nome."</th>";
-	echo "<th>".$sobrenome."</th>";
-	echo "<th>".$sexo."</th>";
-	echo "</tr>";
-
+    echo "<tr>";
+    echo "<td>".$nome."</td>";
+    echo "<td>".$sobrenome."</td>";
+    echo "<td>".$sexo."</td>";
+    echo "</tr>";
 }
+
 mysqli_close($strcon);
 echo "</table>";
-
 ?>
